6006d0dd5ca5d3acec84640abf73a7651a4ddd63
Jesus Velazquez <jesusfvelazquez@hotmail.com>
2023-06-05 20:44:09 +0200

    Redirect to dashboard when clicking logo

--- src/components/base/Layout/NavBar/NavBarResponsive/index.tsx
+++ src/components/base/Layout/NavBar/NavBarResponsive/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { AiOutlineMenu } from "react-icons/ai"
 import { BiBell } from "react-icons/bi"
+import { useNavigate } from "react-router-dom"
 import { useUI } from "../../../../../context/UI/UIContext"
 import { Logo } from "../../../Logo"
 import { Typography } from "../../../Typography"
@@ -8,8 +9,13 @@ import { SearchBar } from "../NavBarComponents/SearchBar"
 import { SearchTitles } from "../NavBarComponents/SearchTitles"
 
 export const NavBarResponsive = () => {
+    const navigate = useNavigate()
     const { setShowWorkInProgress } = useUI()
     const [showSearchBar, setShowSearchBar] = useState<boolean>(false)
+
+    const handleBackToDashboard = () => {
+        navigate('/dashboard')
+    }
 
     return (
         <nav className="w-screen h-full bg-[#161B22] flex flex-col justify-between items-center px-4 py-4">
@@ -24,7 +30,7 @@ export const NavBarResponsive = () => {
                 <Logo
                     type="veryImportant"
                     color="white"
-                    onClick={() => setShowWorkInProgress(true)} />
+                    onClick={handleBackToDashboard} />
                 <Typography
                     text={<BiBell />}
                     type="big"
